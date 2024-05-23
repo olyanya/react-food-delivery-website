27f096b85c299864cad41ed08fa4e65ddfff1e2f
olyanya <you@example.com>
2024-05-23 14:07:33 -0400

    feat: add basic routing for Home, Cart, and PlaceOrder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
