acf0241ee76d564f26e6d395285f71d4531a3e66
olyanya <you@example.com>
2024-05-23 15:42:28 -0400

    feat: added exploreMenu component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Header/Header";
 import "./Home.css";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
