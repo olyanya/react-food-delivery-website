19becea347d191d25b4db1bad9b8067eaa435d17
olyanya <you@example.com>
2024-05-23 13:57:33 -0400

    feat: added navbar component & styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <img className="logo" src={assets.logo} alt="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button> Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
