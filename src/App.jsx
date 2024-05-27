7b29a59fa31bbb47835ca10a6842c1897ff4b270
olyanya <you@example.com>
2024-05-27 14:47:52 -0400

    feat: added Footer component & styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import { assets } from "../../assets/assets";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            voluptate, ut dolorum libero, cumque asperiores beatae, numquam sunt
+            quisquam ipsam saepe porro possimus id at dolorem ad odit! Cumque,
+            cupiditate.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="facebook icon" />
+            <img src={assets.twitter_icon} alt="twitter icon" />
+            <img src={assets.linkedin_icon} alt="linkedin icon" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Private policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-222-222-2222</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
