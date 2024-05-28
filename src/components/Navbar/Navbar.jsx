91908750bff26467176da7d07d1b39238e9857a0
olyanya <you@example.com>
2024-05-28 14:20:57 -0400

    feat: added sign in/sign up components & styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
@@ -44,7 +44,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button> Sign in</button>
+        <button onClick={() => setShowLogin(true)}> Sign in</button>
       </div>
     </div>
   );
